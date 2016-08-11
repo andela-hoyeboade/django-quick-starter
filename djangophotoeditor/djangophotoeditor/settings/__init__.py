96008bda9f47ff6677fcb35f595d99d9d128d669
andela-hoyeboade <hassan.oyeboade@andela.com>
2016-08-11 12:05:23 +0100

    [Feture #128080669] Set up django project

---
+++ djangophotoeditor/djangophotoeditor/settings/__init__.py
@@ -0,0 +1,12 @@
+import os
+
+if not os.getenv('CI') and not os.getenv('HEROKU'):
+    from .development import *
+
+if os.getenv('CI') and not os.getenv('HEROKU'):
+    from django_envie.workroom import convertfiletovars
+    convertfiletovars()
+    from .development import *
+
+if os.getenv('HEROKU') is not None:
+    from .production import *
